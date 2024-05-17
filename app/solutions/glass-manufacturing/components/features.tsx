08bbe5dc89c75608dc370adf98874432d9521158
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2024-05-17 18:10:33 +0530

    added features

---
+++ app/solutions/glass-manufacturing/components/features.tsx
@@ -0,0 +1,17 @@
+export default function Features() {
+  return (
+    <div className=" bg-white py-24 sm:py-32 rounded-3xl ">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-7xl">
+          <h2 className="mt-2 text-3xl font-semibold tracking-tight  leading-10 text-background sm:text-7xl">
+            HappyOps guides you on your way to flexible and more efficient
+            digital manufacturing operations, step by step.
+          </h2>
+        </div>
+        <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
+          <div className="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/solutions/glass-manufacturing/page.tsx
+++ app/solutions/glass-manufacturing/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/hero";
 import Challenges from "./components/challenges";
+import Features from "./components/features";
 
 const Page = () => {
   return (
     <main className=" px-12">
       <Hero />
       <Challenges />
+      <Features />
     </main>
   );
 };
