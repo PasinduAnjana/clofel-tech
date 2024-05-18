2a455460789dc1aff28a2132398644fc186558e0
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2024-05-18 22:07:31 +0530

    added new section

---
+++ app/solutions/glass-manufacturing/components/journey.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Journey = () => {
+  return (
+    <section className="container  min-h-screen px-6 flex items-center justify-center  lg:px-8">
+      <h2 className="text-4xl  font-semibold leading-[176px] tracking-tight text-center flex flex-col  sm:text-[150px]">
+        Your journey to a digital metal factory
+      </h2>
+    </section>
+  );
+};
+
+export default Journey;
--- app/solutions/glass-manufacturing/page.tsx
+++ app/solutions/glass-manufacturing/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./components/hero";
 import Challenges from "./components/challenges";
 import { Features } from "./components/features";
+import Journey from "./components/journey";
 
 const Page = () => {
   return (
     <main className=" px-12">
       <Hero />
       <Challenges />
+      <Journey />
       <Features />
     </main>
   );
